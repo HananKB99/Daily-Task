e0ce60d4fe115b11816bdee4c23890f1635e40bf
HananKB <hanan.basbous99@hotmail.com>
2020-09-20 18:03:49 +0300

    Hanan

--- script.js
+++ script.js
@@ -1,23 +1,47 @@
 
-
- 
- function  add(){
-	
-	if(	$('#input-task').val()===""){
-		alert("“You must write something!”")
-
-	}
-	else {
-
-	var task=$('#list').append("<li>" + $('#input-task').val() + "</li>");
-	$('#input-task').val("")
-
-		 	}
-        }
-
 $("#add-task").on("click", function(){
-    add()
-  })
+   if(	$('#input-task').val()===""){
+		alert("“You must write something!”")
+    }else{
+		var task = $("#input-task").val()
+		$("#list").append("<li> "+ task +" </li>")
+		var desc = $("#input-desc").val()
+		$("#list").append(desc+"<br>")
+		var date = $("#date").val()
+		$("#list").append( date)
+//		$("#list").append("<button > done </button>"+"<br><br>")
+//		$tweet.attr("class", "users")
+		$("#list").append("<input type='button' id='done' value='done' onclick='ddelete()'>")
 
+		
+		
+		console.log(task)
+	}
+	
+	
+  })
+function ddelete (){
+
+  document.getElementById("done").style.textDecoration = " line-through";;
+
+
+}
+////
+// var x = $("#list").append("<button > done </button>"+"<br><br>")
+//'
+/*
+$("#done").on("click", function(){
+	  document.getElementsById("tassk").style.textDecoration = " line-through";
+
+}
+//function ddelete (){
+//
+//  document.getElementsByName("li").style.textDecoration = " line-through";
+//
+//
+//}
+////
+// var x = $("#list").append("<button > done </button>"+"<br><br>")
+//
 
 
