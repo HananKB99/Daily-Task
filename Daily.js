a081bc263413ee9821c215552aa5564980f82063
HananKB <hanan.basbous99@hotmail.com>
2020-09-21 11:29:52 +0300

    before submit

--- Daily.js
+++ Daily.js
@@ -1,3 +1,8 @@
+
+function  ddelet(){
+document.getElementsById("taskc").style.textDecoration="line-through";
+}
+
 //when we click the add button the values will be saved in variables and display below in a list
 $('#add-task').on('click', function(){
 	
@@ -7,26 +12,28 @@ $('#add-task').on('click', function(){
     }else{
 		
 		//the task timing
+		var min =  $("#min").val()
 		var houre = $("#houre").val()
-		$("#list").append(houre+":")
-			var min =  $("#min").val()
-		$("#list").append( min+"<br>")
+		$("#list").append("<br><br>"+houre+":"+min)
+			
+//		$("#list").append( min+"<br>")
 		
 		
 		//the task name will be saved in a variable called task
 		var task = $("#input-task").val()
-		$("#list").append("<li class='taskc'> "+ task +" </li>")
+		$("#list").append("<br><b id='taskc'> "+ task +" </b>")
+		
 		
 		
 		
 		
 		//the task description will be saved in a variable called desc
 		var desc = $("#input-desc").val()
-		$("#list").append(desc+"<br>")
+		$("#list").append("<br>"+desc+"<br><br>")
+//		$desc.attr("class","descc")
 		
 		
-//		var date = $("#date").val()
-//		$("#list").append( date)
+
 		
 //		this button should put a line through the task that was finished
 		$("#list").append('<input type="button" id="done" value="done" onclick="ddelet()">')
@@ -35,14 +42,87 @@ $('#add-task').on('click', function(){
   })
 // this function should put a line through the task that was finished
 //not workink
-function ddelet(){
-document.getElementsByClassName('taskc').style.textDecoration="line-through";
-}
+
 
 // this function should clear and delete all the task when we click on clear all the tasks button
 //not workink
-function clear(){
-	 $("#clear").click(function(){
-    $("#divlist").remove();
-  });
+
+
+
+$('#clear').on('click', function(){
+	$("#list").remove();
+	
+ }
+)
+
+
+var myOptions = {
+    val1 : 1,
+    val2 : 2,
+    val3 : 3,
+    val4 : 4,
+    val5 : 5,
+    val6 : 6,
+    val7 : 7,
+    val8 : 8,
+    val9 : 9,
+    val10 : 10,
+    val11 : 11,
+    val12 : 12,
+    val13 : 13,
+    val14 : 14,
+    val15 : 15,
+    val16 : 16,
+    val17 : 17,
+    val18 : 18,
+    val19 : 19,
+    val20 : 20,
+    val21 : 21,
+    val22 : 22,
+    val23 : 23,
+    val24 : 24
+};
+var hours = $('#houre');
+$.each(myOptions, function(val, text) {
+    hours.append(
+        $('<option></option>').val(val).html(text)
+    );
+});
+//
+//function h (){
+//	var obj={}
+//	for(var i = 1; i<=24 ; i++){
+//	for (var key in obj){
+//		obj[key] = i
+//	}
+//	}
+//	return obj
+//	
+//}
+function min(){
+	for (var i = 1; i<=24 ; i++){
+		$("section").append("<option>"+desc+"</option>")
+	}
 }
+
+//var taskArray =$('#list li')
+//  var last=listitem[listitem.length -1]
+//  if(listitem.length === 0){
+//    alert("There are no tasks to remove!")
+//  }
+//  else
+//  last.remove()
+//}
+
+
+
+				  
+
+
+
+
+
+
+
+
+
