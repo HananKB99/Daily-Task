4c98d5cca1b47db65b86e030581cfd5199908713
HananKB <hanan.basbous99@hotmail.com>
2020-09-21 09:09:54 +0300

    after 6 works

--- Daily.js
+++ Daily.js
@@ -1,25 +1,48 @@
-//function sum (){
-//	return "g"
-//}
-//document.getElementById("newTaskForm");\
+//when we click the add button the values will be saved in variables and display below in a list
+$('#add-task').on('click', function(){
+	
+	// if the input text is empty alert will show up
+   if(	$('#input-task').val()===""){
+		alert("“You must write something!”")
+    }else{
+		
+		//the task timing
+		var houre = $("#houre").val()
+		$("#list").append(houre+":")
+			var min =  $("#min").val()
+		$("#list").append( min+"<br>")
+		
+		
+		//the task name will be saved in a variable called task
+		var task = $("#input-task").val()
+		$("#list").append("<li class='taskc'> "+ task +" </li>")
+		
+		
+		
+		
+		//the task description will be saved in a variable called desc
+		var desc = $("#input-desc").val()
+		$("#list").append(desc+"<br>")
+		
+		
+//		var date = $("#date").val()
+//		$("#list").append( date)
+		
+//		this button should put a line through the task that was finished
+		$("#list").append('<input type="button" id="done" value="done" onclick="ddelet()">')
+	}
 
-
-
-
-
-
-
-function add(){
-	if(	$('#taskname').val()===""){
-				alert("“You must write something!”")
-
-			}
-			else {
-			var task=	$('#list').append("<li>" + $('#taskname').val() + "</li>");
-				$('#taskname').val("")
-		  	}
+  })
+// this function should put a line through the task that was finished
+//not workink
+function ddelet(){
+document.getElementsByClassName('taskc').style.textDecoration="line-through";
 }
 
-$("#addButton").on("click", function(){
-    add()
-  })
+// this function should clear and delete all the task when we click on clear all the tasks button
+//not workink
+function clear(){
+	 $("#clear").click(function(){
+    $("#divlist").remove();
+  });
+}
--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-
+//when we click the add button the values will be saved in variables and display below in a list
 $("#add-task").on("click", function(){
+	
+	// if the input text is empty alert will show up
    if(	$('#input-task').val()===""){
 		alert("“You must write something!”")
     }else{
+		//the task name will be saved in a variable called task
 		var task = $("#input-task").val()
-		$("#list").append("<li> "+ task +" </li>")
+		$("#list").append("<li class='taskc'> "+ task +" </li>")
+		
+		//the task description will be saved in a variable called desc
 		var desc = $("#input-desc").val()
 		$("#list").append(desc+"<br>")
+		
+		
 		var date = $("#date").val()
 		$("#list").append( date)
-//		$("#list").append("<button > done </button>"+"<br><br>")
-//		$tweet.attr("class", "users")
-		$("#list").append("<input type='button' id='done' value='done' onclick='ddelete()'>")
+//		
+		$("#list").append("<input type='button' id='done' value='done' onclick='ddelet()'>")
 
 		
 		
@@ -20,28 +26,12 @@ $("#add-task").on("click", function(){
 	
 	
   })
-function ddelete (){
+function ddelet(){
 
-  document.getElementById("done").style.textDecoration = " line-through";;
-
+document.getElementsByClassName('taskc').style.textDecoration="line-through";
 
 }
 ////
 // var x = $("#list").append("<button > done </button>"+"<br><br>")
-//'
-/*
-$("#done").on("click", function(){
-	  document.getElementsById("tassk").style.textDecoration = " line-through";
-
-}
-//function ddelete (){
 //
-//  document.getElementsByName("li").style.textDecoration = " line-through";
-//
-//
-//}
-////
-// var x = $("#list").append("<button > done </button>"+"<br><br>")
-//
-
 
